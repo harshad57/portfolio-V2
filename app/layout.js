547fc7c3750ff450873d39b658004cdcea56a6ba
Harshad Solkar <146346826+harshad57@users.noreply.github.com>
2025-10-17 13:17:23 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -39,6 +39,7 @@ export default function RootLayout({ children }) {
         {!loading && children}
         </ThemeProvider>
       </body>
+<script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="Harshad57" data-description="Support me on Buy me a coffee!" data-message="Thanks for visiting !" data-color="#FF5F5F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
     </html>
   );
 }
